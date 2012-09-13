8239f485f7f2cd8bce24e54e6c677b3dd0933d77
Hannah Novak <shawncpu5@msn.com>
2012-09-13 17:21:37 +0000

    raaayyy
    git-tfs-id: [https://shawnnovak.visualstudio.com/DefaultCollection]$/Default;C6

--- NC/NC.UI/frmArrays.Designer.cs
+++ NC/NC.UI/frmArrays.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.btnCreateArray = new System.Windows.Forms.Button();
             this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnArrayFor = new System.Windows.Forms.Button();
+            this.btn2dArray = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCreateArray
             // 
-            this.btnCreateArray.Location = new System.Drawing.Point(39, 71);
+            this.btnCreateArray.Location = new System.Drawing.Point(39, 35);
             this.btnCreateArray.Name = "btnCreateArray";
             this.btnCreateArray.Size = new System.Drawing.Size(75, 23);
             this.btnCreateArray.TabIndex = 0;
@@ -50,11 +52,33 @@
             this.lstOutput.Size = new System.Drawing.Size(120, 95);
             this.lstOutput.TabIndex = 1;
             // 
+            // btnArrayFor
+            // 
+            this.btnArrayFor.Location = new System.Drawing.Point(39, 64);
+            this.btnArrayFor.Name = "btnArrayFor";
+            this.btnArrayFor.Size = new System.Drawing.Size(75, 23);
+            this.btnArrayFor.TabIndex = 2;
+            this.btnArrayFor.Text = "1 Dim Array";
+            this.btnArrayFor.UseVisualStyleBackColor = true;
+            this.btnArrayFor.Click += new System.EventHandler(this.btnArrayFor_Click);
+            // 
+            // btn2dArray
+            // 
+            this.btn2dArray.Location = new System.Drawing.Point(39, 93);
+            this.btn2dArray.Name = "btn2dArray";
+            this.btn2dArray.Size = new System.Drawing.Size(75, 23);
+            this.btn2dArray.TabIndex = 3;
+            this.btn2dArray.Text = "1 Dim Array";
+            this.btn2dArray.UseVisualStyleBackColor = true;
+            this.btn2dArray.Click += new System.EventHandler(this.btn2dArray_Click);
+            // 
             // frmArrays
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(382, 236);
+            this.Controls.Add(this.btn2dArray);
+            this.Controls.Add(this.btnArrayFor);
             this.Controls.Add(this.lstOutput);
             this.Controls.Add(this.btnCreateArray);
             this.Name = "frmArrays";
@@ -67,6 +91,8 @@
 
         private System.Windows.Forms.Button btnCreateArray;
         private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnArrayFor;
+        private System.Windows.Forms.Button btn2dArray;
     }
 }
 
--- NC/NC.UI/frmArrays.cs
+++ NC/NC.UI/frmArrays.cs
@@ -25,5 +25,35 @@ namespace NC.UI
             foreach (double number in numbers)
                 lstOutput.Items.Add(number.ToString());
         }
+
+        private void btnArrayFor_Click(object sender, EventArgs e)
+        {
+            // Statically create and array of doubles
+            double[] numbers = new double[4];
+
+            // Set the values
+            numbers[0] = 2.4;
+            numbers[1] = 1.2;
+            numbers[2] = 5.66;
+            numbers[3] = 7;
+
+            // Enumerate and output the contents of that array
+            for (int iCnt = 0; iCnt < 4; iCnt++)
+                lstOutput.Items.Add(numbers[iCnt].ToString());
+        }
+
+        private void btn2dArray_Click(object sender, EventArgs e)
+        {
+            // Create a 2d array
+            double[,] numbers = new double[2, 3];
+
+            numbers[0, 0] = 2.3;
+            numbers[0, 1] = 7.8;
+            numbers[0, 2] = 5.9;
+            numbers[1, 0] = 1.9;
+            numbers[1, 1] = 9.4;
+            numbers[1, 2] = 10.4;
+
+        }
     }
 }
