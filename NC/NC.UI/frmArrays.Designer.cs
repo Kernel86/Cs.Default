9154aa4df9302ce8e79bc090a9f964bf7ef2691d
Hannah Novak <shawncpu5@msn.com>
2012-09-13 16:59:19 +0000

    Initial Commit
    git-tfs-id: [https://shawnnovak.visualstudio.com/DefaultCollection]$/Default;C5

---
+++ NC/NC.UI/frmArrays.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace NC.UI
+{
+    partial class frmArrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateArray = new System.Windows.Forms.Button();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCreateArray
+            // 
+            this.btnCreateArray.Location = new System.Drawing.Point(39, 71);
+            this.btnCreateArray.Name = "btnCreateArray";
+            this.btnCreateArray.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateArray.TabIndex = 0;
+            this.btnCreateArray.Text = "1 Dim Array";
+            this.btnCreateArray.UseVisualStyleBackColor = true;
+            this.btnCreateArray.Click += new System.EventHandler(this.btnCreateArray_Click);
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(212, 35);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(120, 95);
+            this.lstOutput.TabIndex = 1;
+            // 
+            // frmArrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 236);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnCreateArray);
+            this.Name = "frmArrays";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateArray;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
---
+++ NC/NC.UI/frmArrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NC.UI
+{
+    public partial class frmArrays : Form
+    {
+        public frmArrays()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateArray_Click(object sender, EventArgs e)
+        {
+            // Statically create and array of doubles
+            double[] numbers = { 2.4, 1.2, 5.66, 7 };
+
+            // Enumerate and output the contents of that array
+            foreach (double number in numbers)
+                lstOutput.Items.Add(number.ToString());
+        }
+    }
+}
