a5ff01f4b7d2c58c47ef8a0e423fc662c9dae691
Hannah Novak <shawncpu5@msn.com>
2012-10-03 20:12:21 +0000

    Moved Location Service to Class.
    git-tfs-id: [https://shawnnovak.visualstudio.com/DefaultCollection]$/Default;C9

---
+++ GPSTestApp/GPSTestApp.Location.Services/CLocation.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+
+namespace GPSTestApp.Location.Services
+{
+    public class CLocation
+    {
+        // Public Properties
+
+        // Private Properties
+        private GeoCoordinateWatcher _watcher;
+
+        // Constructors
+        public CLocation()
+        {
+            _watcher = new GeoCoordinateWatcher();
+        }
+
+        public CLocation(GeoPositionAccuracy vAccuracy)
+        {
+            _watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+            _watcher.StatusChanged += _StatusChanged;
+            _watcher.PositionChanged += _PositionChanged;
+        }
+
+        // Events
+        public event EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>> PositionChanged;
+        private void _PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            PositionChanged(sender, e);
+        }
+
+        public event EventHandler<GeoPositionStatusChangedEventArgs> StatusChanged;
+        private void _StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            StatusChanged(sender, e);
+        }
+
+        // Public Enums
+        // Enum for globe planes
+        public enum Plane
+        {
+            Horizontal,
+            Vertical
+        }
+
+        // Public Methods
+        // Start Watcher
+        public void Start()
+        {
+            _watcher.Start();
+        }
+
+        // Convert decimal coordinates to formatted degrees
+        public string decimalToDegree(double coordinate, Plane plane)
+        {
+            string pole = String.Empty;
+
+            // Get compass direction
+            if (plane == Plane.Horizontal)
+            {
+                // 90(N)-0-90(S)
+                if (coordinate > 0)
+                    pole = "N";
+                else if (coordinate < 0)
+                    pole = "S";
+            }
+            else if (plane == Plane.Vertical)
+            {
+                // 180(E)-0-180(W)
+                if (coordinate > 0)
+                    pole = "E";
+                else if (coordinate < 0)
+                    pole = "W";
+            }
+
+            // Degrees are the integer portion of the coordinate
+            int degree = (int)coordinate;
+            // Total seconds are the decimal porttion of the coordinate times 3600, the number of seconds in a degree
+            double totalseconds = -1 * (coordinate - degree) * 3600;
+            // Minutes
+            int minutes = (int)(totalseconds / 60);
+            double seconds = totalseconds - minutes * 60;
+
+            // Return the formatted coordinate
+            return pole + " " + degree.ToString() + "°" + minutes.ToString() + "'" + seconds.ToString("N3") + "\"";
+        }
+    }
+}
--- GPSTestApp/GPSTestApp.UI/frmGPSMain.cs
+++ GPSTestApp/GPSTestApp.UI/frmGPSMain.cs
@@ -17,76 +17,36 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Device.Location;
 
+using GPSTestApp.Location.Services;
+using GPSTestApp.GPS;
+
 namespace GPSTestApp.UI
 {
     public partial class frmGPSMain : Form
     {
-        GeoCoordinateWatcher _watcher;
+        CLocation _location; // Location Services
 
         public frmGPSMain()
         {
             InitializeComponent();
-
         }
 
         private void btnGo_Click(object sender, EventArgs e)
         {
-            // Configure Watcher
-            if (_watcher == null)
+            // Configure Location Class
+            if (_location == null)
             {
-                _watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
-                _watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(_watcher_StatusChanged);
-                _watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(_watcher_PositionChanged);
+                _location = new CLocation(GeoPositionAccuracy.High);
+                _location.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(_location_StatusChanged);
+                _location.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(_location_PositionChanged);
             }
 
-            // Start _watcher
-            _watcher.Start();
-        }
-
-        // Enum for globe planes
-        private enum Plane
-        {
-            Horizontal,
-            Vertical
-        }
-
-        // Convert decimal coordinates to formatted degrees
-        private string decimalToDegree(double coordinate, Plane plane)
-        {
-            string pole = String.Empty;
-
-            // Get compass direction
-            if (plane == Plane.Horizontal)
-            {
-                // 90(N)-0-90(S)
-                if (coordinate > 0)
-                    pole = "N";
-                else if (coordinate < 0)
-                    pole = "S";
-            }
-            else if (plane == Plane.Vertical)
-            {
-                // 180(E)-0-180(W)
-                if (coordinate > 0)
-                    pole = "E";
-                else if (coordinate < 0)
-                    pole = "W";
-            }
-
-            // Degrees are the integer portion of the coordinate
-            int degree = (int)coordinate;
-            // Total seconds are the decimal porttion of the coordinate times 3600, the number of seconds in a degree
-            double totalseconds = -1 * (coordinate - degree) * 3600;
-            // Minutes
-            int minutes = (int)(totalseconds / 60);
-            double seconds = totalseconds - minutes * 60;
-
-            // Return the formatted coordinate
-            return pole + " " + degree.ToString() + "°" + minutes.ToString() + "'" + seconds.ToString("N3") + "\"";
+            // Start _location
+            _location.Start();
         }
 
         // Handle location services status changes
-        private void _watcher_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        private void _location_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
         {
             switch (e.Status)
             {
@@ -103,7 +63,7 @@ namespace GPSTestApp.UI
         }
 
         // Handle location change updates
-        private void _watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        private void _location_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
         {
             lblStatus.Text = String.Empty;
 
@@ -114,8 +74,8 @@ namespace GPSTestApp.UI
             {
                 // Display values
                 lblTime.Text = e.Position.Timestamp.ToString();
-                lblLat.Text = decimalToDegree(e.Position.Location.Latitude, Plane.Horizontal);
-                lblLong.Text = decimalToDegree(e.Position.Location.Longitude, Plane.Vertical);
+                lblLat.Text = _location.decimalToDegree(e.Position.Location.Latitude, CLocation.Plane.Horizontal);
+                lblLong.Text = _location.decimalToDegree(e.Position.Location.Longitude, CLocation.Plane.Vertical);
 
                 if (e.Position.Location.VerticalAccuracy.ToString() == "NaN")
                     lblAccuracy.Text = "0 m";
